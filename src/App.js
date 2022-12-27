1bc7b87911a0a0be179712a4826e39e6f949a4c9
ardabagce <ardabagce@gmail.com>
2022-12-27 21:24:13 +0300

    kinda initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import BeerComponent from './Components/beer';
+import Navigation from './Components/navigation';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+function useData() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get('https://random-data-api.com/api/v2/beers')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        // handle errors
+      });
+  }, []);
+  return data;
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAxqsoNnEvj0fMFIVCpBYIGI6jq8qRf-gU",
+    authDomain: "ardaregister-18d82.firebaseapp.com",
+    projectId: "ardaregister-18d82",
+    storageBucket: "ardaregister-18d82.appspot.com",
+    messagingSenderId: "770280114552",
+    appId: "1:770280114552:web:dc39205eeed1bf6c8ce08d",
+    measurementId: "G-CPQ56BXP1V"
+  };
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+}
 
 function App() {
+  const data = useData();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation/>
+      <BeerComponent data={data}/>
     </div>
   );
 }
